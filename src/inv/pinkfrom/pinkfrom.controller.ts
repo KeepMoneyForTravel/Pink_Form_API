22506f15a0c6f3bca4e7c0e545b95418882aec11
WTha0623 <86102410+WTha0623@users.noreply.github.com>
2025-07-13 14:05:15 +0700

    inv7

--- src/inv/pinkfrom/pinkfrom.controller.ts
+++ src/inv/pinkfrom/pinkfrom.controller.ts
@@ -85,14 +85,15 @@ export class PinkfromController {
             const lastRef = await this.pinkfromService.GetpinkfromDesc(comcode);
             let resa: string = '';
             if (lastRef) {
-                const prefix = lastRef.slice(0, 4);
-                const numericPart = lastRef.slice(4);
-                const incrementedNumber = (parseInt(numericPart) + 1).toString().padStart(numericPart.length, '0');
-                resa = prefix + incrementedNumber;
+                // const prefix = lastRef.slice(0, 4);
+                // const numericPart = lastRef.slice(4);
+                // const incrementedNumber = (parseInt(numericPart) + 1).toString().padStart(numericPart.length, '0');
+                // resa = prefix + incrementedNumber;
+                resa = 'asdadasdsdsdsadasd'
             } else {
                 resa = await this.pinkfromService.Getrefid(comcode);
             }
-            return { newRef: resa };
+            return { newRef: resa, last: lastRef };
             // const foundPinkfrom = await this.pinkfromService.getPinkfromByOneNoComp(refno);
             // const foundPinkHinv = await this.hinvService.getPinkHinvfromByOneNoComp(refno);
             // const foundPinkEinv = await this.einvService.GetPinkEinvbyoneNoComp(refno);
